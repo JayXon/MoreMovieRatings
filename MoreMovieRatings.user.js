6cbec49018f0315bd743344bd848feef53e5b4b2
JayXon <jayxon@gmail.com>
2015-10-28 23:41:05 -0700

    New style for new Douban website

--- MoreMovieRatings.user.js
+++ MoreMovieRatings.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MoreMovieRatings
 // @namespace    http://www.jayxon.com/
-// @version      0.2.2
+// @version      0.2.3
 // @description  Show IMDb ratings on Douban, and vice versa
 // @description:zh-CN 豆瓣和IMDb互相显示评分
 // @author       JayXon
@@ -12,8 +12,8 @@
 
 (function() {
     var host = location.hostname;
-    if (host === "movie.douban.com") {
-        var id = document.querySelector("#info a[href^='http://www.imdb.com/']");
+    if (host === 'movie.douban.com') {
+        var id = document.querySelector('#info a[href^="http://www.imdb.com/"]');
         if (!id) {
             return;
         }
@@ -21,26 +21,31 @@
         // Have to use GM_xmlhttpRequest to get http json on https webpage
         GM_xmlhttpRequest({
             method: 'GET',
-            url: "http://www.omdbapi.com/?i=" + id + "&tomatoes=true",
+            url: 'http://www.omdbapi.com/?i=' + id + '&tomatoes=true',
             onload: function(response) {
                 var data = JSON.parse(response.responseText);
                 var ratings = document.createElement('div');
+                ratings.style.padding = '15px 0';
+                ratings.style.borderTop = '1px solid #eaeaea';
                 var sectl = document.getElementById('interest_sectl');
                 sectl.insertBefore(ratings, document.querySelector('.rating_wrap').nextSibling);
                 // IMDb
-                if (data.imdbRating !== "N/A") {
+                if (data.imdbRating !== 'N/A') {
                     ratings.insertAdjacentHTML('beforeend',
-                        "<div style='-webkit-filter: hue-rotate(90deg); filter: hue-rotate(90deg)'>" +
-                        "<br><strong style=color:red class='ll'>IMDb:</strong>" +
-                        "<br><span class='ll bigstar" + 5 * Math.round(data.imdbRating) + "'></span>" +
-                        "<strong class='ll rating_num'>" + data.imdbRating + "</strong></div>" +
-                        "<br>(<a href=http://www.imdb.com/title/" + id + "/ratings target=_blank>" + data.imdbVotes.replace(/,/g, '') + "人评价</a>)"
+                        '<div class="rating_logo">IMDb评分</div>' +
+                        '<div class="rating_self clearfix">' +
+                            '<strong class="ll rating_num">' + data.imdbRating + '</strong>' +
+                            '<div class="rating_right">' +
+                                '<div class="ll bigstar' + 5 * Math.round(data.imdbRating) + '"></div>' +
+                                '<div style="clear: both" class="rating_sum"><a href=http://www.imdb.com/title/' + id + '/ratings target=_blank>' + data.imdbVotes.replace(/,/g, '') + '人评价</a></div>' +
+                            '</div>' +
+                        '</div>'
                     );
                     // Check for IMDb Top 250
                     if (data.imdbRating >= 8) {
                         GM_xmlhttpRequest({
                             method: 'GET',
-                            url: "http://app.imdb.com/chart/top",
+                            url: 'http://app.imdb.com/chart/top',
                             onload: function(top) {
                                 var list = JSON.parse(top.responseText).data.list.list;
                                 var number = function() {
@@ -53,8 +58,8 @@
                                     return;
                                 // inject css if needed
                                 if (document.getElementsByClassName('top250').length === 0) {
-                                    var style = document.createElement("style");
-                                    style.innerHTML = ".top250{background:url(https://s.doubanio.com/f/movie/f8a7b5e23d00edee6b42c6424989ce6683aa2fff/pics/movie/top250_bg.png) no-repeat;width:150px;font:12px Helvetica,Arial,sans-serif;margin:5px 0;color:#744900}.top250 span{display:inline-block;text-align:center;height:18px;line-height:18px}.top250 a,.top250 a:link,.top250 a:hover,.top250 a:active,.top250 a:visited{color:#744900;text-decoration:none;background:none}.top250-no{width:34%}.top250-link{width:66%}";
+                                    var style = document.createElement('style');
+                                    style.innerHTML = '.top250{background:url(https://s.doubanio.com/f/movie/f8a7b5e23d00edee6b42c6424989ce6683aa2fff/pics/movie/top250_bg.png) no-repeat;width:150px;font:12px Helvetica,Arial,sans-serif;margin:5px 0;color:#744900}.top250 span{display:inline-block;text-align:center;height:18px;line-height:18px}.top250 a,.top250 a:link,.top250 a:hover,.top250 a:active,.top250 a:visited{color:#744900;text-decoration:none;background:none}.top250-no{width:34%}.top250-link{width:66%}';
                                     document.head.appendChild(style);
                                 }
                                 document.querySelector('h1').insertAdjacentHTML('beforebegin', '<div class="top250"><span class="top250-no">No.' + number + '</span><span class="top250-link"><a href="http://www.imdb.com/chart/top">IMDb Top 250</a></span></div>');
@@ -67,25 +72,27 @@
                 }
 
                 // Metascore
-                if (data.Metascore !== "N/A") {
+                if (data.Metascore !== 'N/A') {
                     var metascore = parseInt(data.Metascore);
                     var metacolor;
                     if (metascore >= 60) {
-                        metacolor = "#6c3";
+                        metacolor = '#6c3';
                     } else if (metascore >= 40) {
-                        metacolor = "#fc3";
+                        metacolor = '#fc3';
                     } else {
-                        metacolor = "#f00";
+                        metacolor = '#f00';
                     }
                     ratings.insertAdjacentHTML('beforeend',
-                        "<br>Metascore: <span style='height: 24px; width: 24px; line-height: 24px; background-color: " + 
-                        metacolor + "; color: #fff; vertical-align: middle; display: inline-block; text-align: center; font-weight: bold'>" + data.Metascore + "</span>"
+                        'Metascore: ' +
+                        '<span style="background-color: ' + metacolor + '; color: #fff; height: 24px; width: 24px; line-height: 24px; vertical-align: middle; display: inline-block; text-align: center; font-weight: bold">' +
+                            data.Metascore +
+                        '</span>'
                     );
                 }
 
                 // Rotten Tomatoes
-                if (data.tomatoMeter !== "N/A" || data.tomatoUserMeter !== "N/A") {
-                    ratings.insertAdjacentHTML('beforeend', "<br>");
+                if (data.tomatoMeter !== 'N/A' || data.tomatoUserMeter !== 'N/A') {
+                    ratings.insertAdjacentHTML('beforeend', '<br>');
                     var tomatoimg = {
                         "certified" : "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAEyElEQVR4AdWTY4DkSABGXyXpTqYxRk+PT2sLZ9u2bdu2bdvmeG27b8dWWkhSZxO/7sWo7xX53yP4C/yGGtx7M/f+20xWtivNE5mOBAGicp4zZ+4C53Pg3X8l0DVh3H+695GjDuLwxqjklsd8lBbaDN/Y+vZaP89FcaHDqDyn9dQbkxcBz/xtwaAC19CZz3jm1a4X+gfVOhcfH+G9SjejBqeonaMzYpDFgy96OHi3GMUBG78Oi2fw3lG3mwcAsT8VDP4m/FV97pd9GB/WGDS1KRQVSE4+IMxDs3OZEjTJVGwkMHJQikPPz+SW80z6TMGSma7lwNA/FBiaMFY+m9V307uanpUuOfmgKO9U6l+HR4lZKue9HKQs6HDqZm3oikN/WLAipHHZPX5KAjbnHh7htefFPcCZfI/Gzzh3d//NxaNj+hYtBg++4qG8THLkPjGqm7O5e3ohfQv6aGkQxNI34ooxK8nwwdANbIZtlGLUJjajh1sEjzHOGHXowE1A629a0Pt2Vuym9zRji/EpSgocKsocLvyigh7Nz4LVCTxzenAhMXYsYJdglHHlvWwV6MaMCl76OI0PanROODDCp2/LH1vxo2Byhb5Z9XPu2qNu9JHhl1xwdJgQmTxYE6AkIVmUpaFKgcsFAUUwfrrJ/CIXnjGCC8vX8dS7GmZEECxwGO6jFQj+QnDqlhlX335H8goFAaqksdbLxx/ks9faGHqGxsW7ZeCNSbRei/WjvJz2Qi+jVsWYm6XSMtTNTic30tCnMHyjFMl13h+zNX5cUEjHUnCkgHZB5u1+DvYluW+cQXxsBkWmxa5zTQY1WNwmFELFGsPXCEYOOIysj6MZfoacY2IlVaTb4TeDLEAIW0UCqToDxxaIPJWzvQauiAuJRqo4RSoe46DP+5kxwYcEpABUSHyShn52GGwBUe23gvqQtU5J6EgNFFNF1UDRBcKrIoJ+SElEdwzFLSiwYKMVMRQBErAERBQHY5WOXurQFib+G8H8hvgnyoCXL9bGsRokVliiNMdRZBy1O4qqqCTaTGhPIWLg9Fnca6cYprkwHQdHKuQuTRKIChbPEp/8RhBOOD33vas0dRZdUrzDhdvyxGMPc8TRx4G0KdtgI6ZOHE9HcwPX3HgTjz76KG9VVhOq+ozLH36IpfPn49EVojc4HHPErjQ3zO35jaB4WP7F9WPLi70rY8xpNJm9cAVjmsMYHi+azCXc20V1kyTUESPl7uHFV98jsMlwTDNBTTPsd9Il335vcATqpOBRwNE/CrICaUcUb2js3dJuo/TbrP3oCzq7ljKz3WL9nDcZ1OlQvtmevL8uwsKuJPH+TGa0xhk+qBB/YQXvrYvy8hM3EdjvEtpMSVZMEl7acefCytZLRGbAc9wxD41+tDeUojIkiX/cCgKMybnEpnei+DUc00IIkFIihIIQgmQq+e2zy+OGpETNMbC/mQRZBsVjM9huTx+rq8IIAN3jOmT/28c//1FlBDuRAkBLOM1OOBni7yBxlOy0LSxVQqZOsSLJlomFdU+v3kkD8Aa8g1r7QF3aRv/KvhoBQ4Bi/gG2Y5+lKNrx/lxPhdgukJbwGkMmHD+iDQD/oLxPfGWZ1wA6/Hc86d7di7fccJbb5R7N/56vAEDvDGwghbBBAAAAAElFTkSuQmCC",
                         "fresh"     : "iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAFBElEQVR4Aa2VA7TszBpEd6c7yfiY13q2bdu2bdu2bdu2bdtHGGei7u9l8Ju4tVaNsKtaieJsyA917djNjrzk9z/a/hzwKc6BDGdDpbo/e3i1es3NYPtrrX+1K0B0XAI8rUJtvKA6E+6J/9XV57/I9PX/mGX/A77D2ZQ6xRuFBzgAE+r6ngvO3ODoFVfuV58LyvZPvSs1zt+wP/3W/56YRPnuf3698xGgc45GIChvam/1KqsXnLnYwQvN3nTPhWeulXStIXO0kjb/+vh/zEVue/AZUvPi1f8kgx+ttT8GJGd7BABTvlm5/NX3vlEuM3/TxaUSTdH8+7d9yv/oUGsY6pecJlguMb8z2M7/3Pvd5k+3Pv/DP+y8D/jXWQYESgXXPDh7p/lbHnhcfqh6QV02DFKo4fhnU6hVNIcWNO4fEUEno1sExg2fcGvw73984l8v/M5vN98CJKcbMOXpmdtecvWljasu3fNif4459p+UPx30+f6NF9ChR9bLiaqG0PeY++uA+759g1rHsjal+d2hkJ9dosLar7ff88Wv/euRwPYpAspKlZ+8uvzWix1o3PFyu5YlrSAsrODpt1vk9/tC7vyONbpW+MpN5hkcrnCjLzS5ywe2yX2FlwstDd89EvCTne6HX/aX/90DiE4MeHh95glPm5t7fqWqyBYNa4s+67OGv84FfPlgmUN/6/O4r7ao/zvn+0XjN99/menA48kv/C9hIuRG4TswuaPvK17YbT4NeDaAdwE/uPADGtMPV3VNuuwTzfv8YUbzw1mffxrFdX/X5cH/TPCXA9pHQq7Ycdzxg9sMrBCXNZ5jpMyDQRHqKcV9a1MP/fygf/HCmJuUardaDM2yqiqkoalO+1y3EXD9IMTzFH7FJ1tIiHQKqdCZN1z5rwl/+GWfqKKY3RUEheIkzXt64abl6q2BX5orh+Wr4wMlUKGCqoaZMt5CHVUyJO2EfLODF+dQUtgSeL7H5X4VQS6Id0o4gAOuGJavCWD2G3MIBSiFGAWeQrRGygGqXoIhWHuFx9+pwkkAB9YzlIJMn4SXCTxFmNFmH4ApKVXCAVYgH1uSDNcejJ/7KZLZwoLkbvw9inIuoECGhglCSIFYhIE4H8A0rdtdzlklFlQkSD9HBTHWOZTWSJpDO0H1LcQCiYCVERg1AU9aJwgRMED4n827AOZ3efLrC2bBRaQn2HaGNuDsMCxHlMLlDgq4a2fQdhAJOBA1BmeT1hHD1kJfHD3n+FUa/xbAfDmOPnvrSv2O0negwUqOShwq9MBTkDskLty20BRcIghCJpw0HQzBhZ2li9DKUr4fDz4zCeh/7odp/N3LB6Ur03GQgnQtEqoTA4hB+oIkQgYjeMKkNePWXWfp2Jx2nvHrOPrRr7Pk0wAK4CrFlnrX3MrHpjxvGgAP0JOvRRAL1o3hyaR1xLCxG8HbtoC7bNR8LYk7n8iy2wJfOsXF7o7Vxj1eOr3w+lCpEsJIMrKQAhlCDEQiI/ecK2xpuXHrVhKzlSXZ16x9MPDm071c36Rcu9XTp+ZecNj4xxyQTxYxnrTuU1gsPetonwDOEpppwj/z/G8/FnkS8MEzveEcMv7R+9amH3bdcuWWs57ZOxA32R1CT9x4ngu38rQAx/w3zf73B3Gf+BO8Cvjj6d9wTkdHjH+BS4Wla1zQhFde0PqCGuaK9n4zy9LNLGn+O0//8A/rvrsG3wB+xxno/+N5rMoDguFXAAAAAElFTkSuQmCC",
